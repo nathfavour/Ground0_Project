5594374e599708b7567eb070e4f541a50cf93122
nathfavour <116535483+nathfavour@users.noreply.github.com>
2023-03-03 11:45:59 +0100

    Update admin.py

--- Nathaniel/admin.py
+++ Nathaniel/admin.py
@@ -1,3 +1,7 @@
+"""This is where the database tables (or referred to
+as models in django) are set to be displayed in the admin page"""
+
+
 from django.contrib import admin
 from .models import Sessions, Employee
 
