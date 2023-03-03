17f87d912e4e1715d698208d3d92c3bee359e6af
nathfavour <116535483+nathfavour@users.noreply.github.com>
2023-03-03 11:49:36 +0100

    Update urls.py

--- Nathaniel/urls.py
+++ Nathaniel/urls.py
@@ -1,3 +1,7 @@
+"""this file makes it possijle for us to locate 
+different views by the urlpatterns configured here"""
+
+
 from django.urls import path
 from . import views
 from .views import register
